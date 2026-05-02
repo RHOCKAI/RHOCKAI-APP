21de5492a0ed259df6d96c677f1bd89b902d66b5
Beinomugisha Innocent <beinomugishainnocent2001@gmail.com>
2026-05-03 00:12:48 +0300

    chore: remove unused imports and final cleanup

--- frontend/lib/features/workout/workout_summary_screen.dart
+++ frontend/lib/features/workout/workout_summary_screen.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import '../camera_ai/session/session_model.dart';
 import '../gamification/data/repositories/gamification_repository.dart';
